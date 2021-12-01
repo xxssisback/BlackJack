4347a513be2224d8676b504fc5d91355d405e508
Sergio Muñoz García <57290458+xxssisback@users.noreply.github.com>
2021-12-01 18:43:02 +0100

    Update game.php

--- assets/functions/game.php
+++ assets/functions/game.php
@@ -4,15 +4,13 @@ $t = new blackjack;
 $t->iniciar();
 session_start();
 
-// Clase BlackJack (Es la clase que de ejecutará)
-class blackjack
-{
+
     
-    private static $_SESSION["jugador"] == 2;
-    private static $jugador1 = [];
-    private static $jugador2 = [];
-    private static $cartas = [2,3,4,5,6,7,8,9,10,11];
-    private static $boo = 0;
+$_SESSION["jugador"] == 2;
+$_SESSION["jugador"]1 = [];
+$_SESSION["jugador"]2 = [];
+$cartas = [2,3,4,5,6,7,8,9,10,11];
+$boo = 0;
 
     
 
@@ -21,7 +19,7 @@ class blackjack
      *  FUNCIONES PRINCIPALES
      */
 
-    private function mesa()
+    function mesa()
     {
         echo ("
         ____________________
@@ -33,13 +31,13 @@ class blackjack
     }
     
     // Esta funcion es la que se encarga de iniciar la partida
-    public function iniciar()
+    function iniciar()
     {  
-        $sumj1 = $this->sumaCartasj1();
-        $sumj2 = $this->sumaCartasj2();
+        $sumj1 = sumaCartasj1();
+        $sumj2 = sumaCartasj2();
         do {
-            $this::$jugador==2?$this::$jugador=1:$this::$jugador=2;
-            $this->mostrarJugador($sumj1, $sumj2);
+            $_SESSION["jugador"] == 1 ? $_SESSION["jugador"] = 2 : $_SESSION["jugador"] = 1;
+            mostrarJugador($sumj1, $sumj2);
             
             echo ("0. ");
             echo ("Salir\n");
@@ -47,26 +45,26 @@ class blackjack
             echo ("Pedir carta\n");
             echo ("2. ");
             echo ("Plantarte\n");
-            $opt = readline("Dime la opcion que quieres elegir jugador " . $this::$jugador . " : ");
+            $opt = readline("Dime la opcion que quieres elegir jugador " . $_SESSION["jugador"] . " : ");
             
             switch ($opt) {
                 case '0':
-                    $this::$boo == 1;
+                    $boo == 1;
                     break;
                 case '1':
-                    $this->lanzarCarta($this::$jugador);
-                    if ($this::$jugador == 1) {
-                        $sumj1 = $this->sumaCartasj1();
-                        $this->mostrarJugador($sumj1, $sumj2);
+                    lanzarCarta($_SESSION["jugador"]);
+                    if ($_SESSION["jugador"] == 1) {
+                        $sumj1 = sumaCartasj1();
+                        mostrarJugador($sumj1, $sumj2);
                     } else {
-                        $sumj2 = $this->sumaCartasj2();
-                        $this->mostrarJugador($sumj1, $sumj2);
+                        $sumj2 = sumaCartasj2();
+                        mostrarJugador($sumj1, $sumj2);
                     }
                     echo("\n");
-                    $this->maxLimite($sumj1, $sumj2);
+                    maxLimite($sumj1, $sumj2);
                     break;
                 case '2':
-                    $this->noHayGanador($sumj1, $sumj2);
+                    noHayGanador($sumj1, $sumj2);
                     break;
                 default:
                     break;
@@ -80,46 +78,46 @@ class blackjack
 
 
     // Esta funcion recibe al ugador y al mesero para poder repartir la carta
-    private function lanzarCarta($jugador){
-        if ($jugador == 1){
-            //$num = rand($this::$cartas[0], $this::$cartas[count($this::$cartas)]);
-            $num = $this::$cartas[rand(0,count($this::$cartas)-1)];
-            array_push($this::$jugador1, $num);
+    function lanzarCarta($_SESSION["jugador"]){
+        if ($_SESSION["jugador"] == 1){
+            //$num = rand($cartas[0], $cartas[count($cartas)]);
+            $num = $cartas[rand(0,count($cartas)-1)];
+            array_push($_SESSION["jugador"] = 1, $num);
         } else {
-            //$num = rand($this::$cartas[0], $this::$cartas[count($this::$cartas)]);
-            $num = $this::$cartas[rand(0,count($this::$cartas)-1)];
-            array_push($this::$jugador2, $num);
+            //$num = rand($cartas[0], $cartas[count($cartas)]);
+            $num = $cartas[rand(0,count($cartas)-1)];
+            array_push($_SESSION["jugador"] = 2, $num);
         }
     }
 
 
-    private function mostrarJugador ($sumj1, $sumj2) {
-        if ($this::$jugador == 1) {
+    function mostrarJugador ($sumj1, $sumj2) {
+        if ($_SESSION["jugador"] == 1) {
             echo("==============================================<br>");
-            echo ("   Jugador " . $this::$jugador . " tu mano es: [");
-            ($this->mostrarMano($this::$jugador));
+            echo ("   Jugador " . $_SESSION["jugador"] . " tu mano es: [");
+            (mostrarMano($_SESSION["jugador"]));
             echo ("] Total: " . $sumj1 . "<br>");
             echo ("==============================================<br><br>");
         } else {
             echo ("==============================================<br>");
-            echo ("   Jugador " . $this::$jugador . " tu mano es: [");
-            ($this->mostrarMano($this::$jugador));
+            echo ("   Jugador " . $_SESSION["jugador"] . " tu mano es: [");
+            (mostrarMano($_SESSION["jugador"]));
             echo  ("] Total: " . $sumj2 . "<br>");
             echo ("==============================================<br><br>");
         }
     }
 
     // mostrar arrays
-    private function mostrarMano($jugador) {
-        if ($jugador == 1) {
-            for ($i=0; $i < count($this::$jugador1); $i++) { 
-                echo ($this::$jugador1[$i]);
-                if($i<count($this::$jugador1)-1) echo ",";
+    function mostrarMano($_SESSION["jugador"]) {
+        if ($_SESSION["jugador"] == 1) {
+            for ($i=0; $i < count($_SESSION["jugador"]1); $i++) { 
+                echo ($_SESSION["jugador"]1[$i]);
+                if($i<count($_SESSION["jugador"]1)-1) echo ",";
             }    
         } else {
-            for ($x=0; $x < count($this::$jugador2); $x++) { 
-                echo ($this::$jugador2[$x]);
-                if($x<count($this::$jugador1)-1) echo ",";
+            for ($x=0; $x < count($_SESSION["jugador"]2); $x++) { 
+                echo ($_SESSION["jugador"]2[$x]);
+                if($x<count($_SESSION["jugador"]1)-1) echo ",";
             } 
         }
         
@@ -127,18 +125,18 @@ class blackjack
 
 
     // Funciones para sumar las cartas de la mano
-    protected function sumaCartasj1(){ 
+    function sumaCartasj1(){ 
         $sumj1 = 0;
-        for ($i = 0; $i < count($this::$jugador1); $i++) {
-            $sumj1 += $this::$jugador1[$i];
+        for ($i = 0; $i < count($_SESSION["jugador"]1); $i++) {
+            $sumj1 += $_SESSION["jugador"]1[$i];
         }
         return $sumj1;
     }
 
-    private function sumaCartasj2(){
+    function sumaCartasj2(){
         $sumj2 = 0;
-        for ($x = 0; $x < count($this::$jugador2); $x++) {
-            $sumj2 += $this::$jugador2[$x];
+        for ($x = 0; $x < count($_SESSION["jugador"]2); $x++) {
+            $sumj2 += $_SESSION["jugador"]2[$x];
         }
         return $sumj2;
     }
@@ -150,33 +148,33 @@ class blackjack
      */
 
     // Esta funcion comprueva que no haya ganador
-    private function noHayGanador($sumj1, $sumj2){
+    function noHayGanador($sumj1, $sumj2){
     if ($sumj1 >= $sumj2) {
             echo (" ¡ GANADR JUGADOR1 !");
             readline();
-            $this::$boo == true;
+            $boo == true;
         } else {
             echo (" ¡ GANADR JUGADOR2 !");
             readline();
-            $this::$boo == true;
+            $boo == true;
         }
     }
 
-    private function maxLimite($sumj1, $sumj2){
+    function maxLimite($sumj1, $sumj2){
         if ($sumj1 > 21 || $sumj2 > 21) {
             if ($sumj1 > 21) {
                 echo ("Jugador1 ha perdido la partida. Pulsa (enter) para continuar");
                 readline();
-                $this::$boo == true;
+                $boo == true;
             } else {
                 echo ("Jugador2 ha perdido la partida. Pulsa (enter) para continuar");
                 readline();
-                $this::$boo == true;
+                $boo == true;
             }
         }
     }
     
-}
+
 
 // Ejecucion del juego
 new blackjack();
